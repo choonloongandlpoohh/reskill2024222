9020a8939ed5b58acea5856106fa3c22519b1cd6
LXbootcamp <162936402+LXbootcamp@users.noreply.github.com>
2024-04-24 16:40:01 +0900

    1

---
+++ java/DBProject/src/test01.java
@@ -0,0 +1,32 @@
+public class test01
+{
+	public static void main(String[] args)
+	{
+		System.out.println("문자열 출력 LX2024");
+		
+		System.out.println("프로그램 시작");
+		
+		int a = 1;
+		int b = 2;
+		int c = a + b;
+		
+		System.out.println("c = " + c);
+		
+		System.out.println("프로그램 끝");
+		
+		String d = "I";
+		String e = " AM ";
+		String f = "a boy";
+		String g = d + e + f;
+		
+		System.out.println("g = " + g);
+		
+		int index = 0;
+		while (index < 100)
+		{
+			System.out.println("while문 안에서 반복 중" + (index + 1));
+			//index++;
+		}
+		
+	}
+}
